7f7d099dd6123986a93005c64dee7172fce8bc55
Ir0nblast3 <tpafonso1@gmail.com>
2026-07-17 17:01:48 +0100

    Starting topbar & hero layouts

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,7 +1,23 @@
 from django.db import models
-
 from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    # HERO
+    hero_title = models.CharField(max_length=100, blank=True)
+    hero_subtitle = models.CharField(max_length=255, blank=True)
+
+    hero_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("hero_title"),
+        FieldPanel("hero_subtitle"),
+        FieldPanel("hero_image"),
+    ]
