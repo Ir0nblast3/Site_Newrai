90f3b8acdf86956ac63a3ebb770d8dd5f9c3a57d
Ir0nblast3 <tpafonso1@gmail.com>
2026-07-23 17:55:33 +0100

    Finishing footer  & starting new section

--- mysite/base/models.py
+++ mysite/base/models.py
@@ -1,6 +1,32 @@
 from django.db import models
 from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.models import Orderable
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+
+
+class SocialsLogo(Orderable):
+    footer = ParentalKey(
+        'base.FooterSettings',
+        on_delete=models.CASCADE,
+        related_name='socials'
+    )
+
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    url = models.URLField()
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel("url"),
+    ]
 
 
 @register_setting
@@ -19,7 +45,7 @@ class NavigationSettings(BaseGenericSetting):
     ]
 
 @register_setting
-class FooterSettings(BaseGenericSetting):
+class FooterSettings(BaseGenericSetting, ClusterableModel):
     
     newRai_icon=models.ForeignKey(
         "wagtailimages.Image",
@@ -39,8 +65,21 @@ class FooterSettings(BaseGenericSetting):
         related_name="+"
     )
 
+    email = models.CharField("Email", max_length=255, blank=True)
+
+    card_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
     panels = [
         FieldPanel("newRai_icon"),
         FieldPanel("developed_by"),
         FieldPanel("finaciamento_icon"),
+        InlinePanel("socials", label="Socials"),
+        FieldPanel("email"),
+        FieldPanel("card_image")
     ]
--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,8 +1,29 @@
 from django.db import models
 from wagtail.models import Page
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.fields import RichTextField
+from modelcluster.fields import ParentalKey
+from wagtail.models import Orderable
 
+
+class Enterprises(Orderable):
+    page = ParentalKey(
+        'HomePage',
+        on_delete=models.CASCADE,
+        related_name='enterprises'
+    )
+
+    enterprise_img = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    panels = [
+        FieldPanel('enterprise_img'),
+    ]
 
 class HomePage(Page):
     # HERO
@@ -24,8 +45,15 @@ class HomePage(Page):
         related_name="+",
     )
 
+    hero_text = RichTextField(
+         blank=True,
+        features=["bold", "italic"]
+    )
+
     content_panels = Page.content_panels + [
         FieldPanel("hero_title"),
         FieldPanel("hero_subtitle"),
         FieldPanel("hero_image"),
+        FieldPanel("hero_text"),
+        InlinePanel("enterprises", label="Enterprises"),
     ]
